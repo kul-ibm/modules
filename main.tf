8868d884535807b821b440a737db7243d25c6471
Kulbhushan <kulbhushan.mayer@>
2021-03-02 15:11:23 +0530

    updated to procure managed servers

--- main.tf
+++ main.tf
@@ -7,10 +7,16 @@ module "createServer" {
 }
 
 module "installAnsible" {
+  depends_on = [ module.createServer ]
   for_each = toset(module.createServer.public_ip)
   source = "./modules/installAnsible"
   host = each.value
   pemFile = "c:/training/kul-labs.pem"
+}
+
+module "createManagedServer" {
+  source = "./modules/procureServer"
+  quantity = 2
 }
 
 output "public-ips" {
